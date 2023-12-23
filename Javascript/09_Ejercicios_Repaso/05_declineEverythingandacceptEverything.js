2d2ed23a03355e3c00d4bc26139cab64fa7fbab4
JackDev21 <jose.a.c.lopez@gmail.com>
2023-12-23 14:37:08 +0100

    squarenums

--- Javascript/09_Ejercicios_Repaso/05_declineEverythingandacceptEverything.js
+++ Javascript/09_Ejercicios_Repaso/05_declineEverythingandacceptEverything.js
@@ -1,20 +1,23 @@
+
+
 const veggies = ['broccoli', 'spinach', 'cauliflower', 'broccoflower'];
 
 const politelyDecline = (veg) => {
     console.log('No ' + veg + ' please. I will have pizza with extra cheese.');
 }
 
-// Write your code here:
 
 const declineEverything = (arr) => {
-    arr.forEach(politelyDecline);
+    arr.forEach(politelyDecline); // Itera sobre cada elemento del array y llama a politelyDecline para cada elemento. El array se pasa como argumento a politelyDecline.
 }
 
+
 const acceptEverything = (arr) => {
+    // Itera sobre cada elemento del array e imprime un mensaje aceptándolo
     arr.forEach(veg => console.log(`Ok, I guess I will eat some ${veg}.`))
 }
 
 
-
 declineEverything(veggies);
+
 acceptEverything(veggies);
