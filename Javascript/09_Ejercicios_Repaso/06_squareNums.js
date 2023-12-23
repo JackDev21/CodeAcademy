2d8869cf4fba1556172dcb067e72341c422778a2
JackDev21 <jose.a.c.lopez@gmail.com>
2023-12-23 15:21:46 +0100

    squareNum

--- Javascript/09_Ejercicios_Repaso/06_squareNums.js
+++ Javascript/09_Ejercicios_Repaso/06_squareNums.js
@@ -4,8 +4,15 @@ const toSquare = (num) => num * num;
 
 // Write your code here:
 
-const squareNums = (numbers) => numbers.map(num => num * num) // const squareNums = (numbers) => numbers.map(toSquare);
+const squareNums = (arr) => arr.map(num => num * num) // const squareNums = (numbers) => numbers.map(toSquare);
 // Usamos map para iterar sobre el array y luego usamos la función toSquare para elevar cada número al cuadrado. 
 //El resultado se almacena en un nuevo array. El array original no se modifica.
-
 console.log(squareNums(numbers));
+
+
+const squareNums2 = numbers.map(num => {
+    return num * num // Diferente forma de hacerlo, itera sobre el array y eleva cada número al cuadrado.
+});
+console.log(squareNums2);
+
+
