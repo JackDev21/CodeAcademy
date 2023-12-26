0da80440346eeb0d4093f975d23e43ef6a284c78
JackDev21 <jose.a.c.lopez@gmail.com>
2023-12-26 11:28:51 +0100

    shoutGreetings

---
+++ Javascript/09_Ejercicios_Repaso/07_shoutGreetings.js
@@ -0,0 +1,12 @@
+
+shoutGreetings = (arr) => {
+    return arr.map(word => word.toUpperCase() + "!");
+}
+
+
+
+const greetings = ['hello', 'hi', 'heya', 'oi', 'hey', 'yo'];
+
+console.log(shoutGreetings(greetings))
+// Should print [ 'HELLO!', 'HI!', 'HEYA!', 'OI!', 'HEY!', 'YO!' ]
+
