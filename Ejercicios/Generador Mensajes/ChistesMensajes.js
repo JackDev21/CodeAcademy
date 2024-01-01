2eaba3670a109a0e0a48332487e5fdf5e2fe4c77
JackDev21 <jose.a.c.lopez@gmail.com>
2024-01-01 19:39:12 +0100

    Creacion de mensajes

---
+++ Ejercicios/Generador Mensajes/ChistesMensajes.js
@@ -0,0 +1,16 @@
+
+
+const mensajesInspiradores = ["El éxito es la suma de pequeños esfuerzos repetidos día tras día.",
+    "El único límite para tus logros es tu propia imaginación.",
+    "Cree en ti mismo y todo será posible."
+]
+
+const chistes = ["¿Por qué los programadores prefieren el café frío? Porque les gusta el Java helado.",
+    "¿Por qué los programadores prefieren el día de la semana? Porque les gusta el lunes.",
+    "Un programador va a la tienda a comprar leche. Su esposa le dice: Si hay huevos, trae seis. El programador vuelve con seis botellas de leche."]
+
+const astrologia = [
+    "Hoy es un día excelente para tomar decisiones importantes.",
+    "Tu creatividad te llevará a grandes logros esta semana.",
+    "Estarás rodeado de energías positivas en el trabajo."
+];
