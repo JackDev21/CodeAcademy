cad2378a8dfd710d627ec20f7d15c65d3d199b47
JackDev21 <jose.a.c.lopez@gmail.com>
2024-01-04 15:21:32 +0100

    Dom Events ejercicio 1 comentarios

--- DOM/Events/01_Ejercicio/main.js
+++ DOM/Events/01_Ejercicio/main.js
@@ -1,10 +1,15 @@
+// Selecciona el elemento con el ID 'read-more' y lo almacena en la variable 'readMore'
 let readMore = document.getElementById('read-more');
+
+// Selecciona el elemento con el ID 'more-info' y lo almacena en la variable 'moreInfo'
 let moreInfo = document.getElementById('more-info');
 
-// Write your code here:
-
+// Define una función de flecha 'showInfo'
 const showInfo = () => {
+    // Cambia la propiedad de estilo 'display' del elemento 'moreInfo' a 'block', haciéndolo visible
     moreInfo.style.display = `block`
 }
 
-readMore.addEventListener('click', showInfo)
+// Añade un manejador de evento 'click' al elemento 'readMore'
+// Al hacer clic en este elemento, se ejecutará la función 'showInfo'
+readMore.addEventListener('click', showInfo);
