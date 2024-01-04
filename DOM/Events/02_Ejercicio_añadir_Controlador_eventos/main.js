e4eec970046ad4bf399d20790cf11a1ec2e8ad67
JackDev21 <jose.a.c.lopez@gmail.com>
2024-01-04 16:30:31 +0100

    events

---
+++ DOM/Events/02_Ejercicio_añadir_Controlador_eventos/main.js
@@ -0,0 +1,35 @@
+let view = document.getElementById('view-button');
+let close = document.getElementById('close-button');
+let codey = document.getElementById('codey');
+
+let open = function () {
+    codey.style.display = 'block';
+    close.style.display = 'block';
+};
+
+let hide = function () {
+    codey.style.display = 'none';
+    close.style.display = 'none';
+};
+
+view.addEventListener('click', open);
+close.addEventListener('click', hide);
+
+// Write your code here
+
+const textChange = () => {
+    view.innerHTML = 'Hello, World!'
+}
+
+const textReturn = () => {
+    view.innerHTML = 'View'
+}
+
+view.addEventListener('click', textChange)
+close.addEventListener('click', textReturn)
+
+
+
+
+
+
