17ac0089d916b34bcc66933fbe6a551488412207
JackDev21 <jose.a.c.lopez@gmail.com>
2024-01-09 19:24:04 +0100

    introducion react

--- React/Introduccion/introduccion.jsx
+++ React/Introduccion/introduccion.jsx
@@ -1,26 +1,22 @@
 // Hola mundo con React
-
 const h1 = <h1>Hello world</h1>;
 
 // Atributos
-
 const p1 = <p id="large">foo</p>;
 const p2 = <p id="small">bar</p>;
 
-//JSX Elementos
-
+// JSX Elementos
 const myTeam = {
-	center: <li>Benzo Walli</li>,
-	powerForward: <li>Rasha Loa</li>,
-	smallForward: <li>Tayshaun Dasmoto</li>,
-	shootingGuard: <li>Colmar Cumberbatch</li>,
-	pointGuard: <li>Femi Billon</li>,
+	center: <li>Benzo Walli</li>, // Jugador en la posición central
+	powerForward: <li>Rasha Loa</li>, // Ala-pívot
+	smallForward: <li>Tayshaun Dasmoto</li>, // Alero
+	shootingGuard: <li>Colmar Cumberbatch</li>, // Escolta
+	pointGuard: <li>Femi Billon</li>, // Base
 };
 
-const myArticle = <article></article>;
+const myArticle = <article></article>; // Elemento artículo vacío
 
 // JSX Anidados
-
 const myDiv = (
 	<div>
 		<h1>Hello world</h1>
@@ -33,25 +29,23 @@ const theExample = (
 	</a>
 );
 
-// Elementos exteriores. La primera etiqueta de apertura y la etiqueta de cierre final de una expresión JSX deben pertenecer al mismo elemento JSX.
-// JSX tiene varios elementos externos, la solución suele ser simple: envuelva la expresión JSX en un elemento <div>.
-
+// Elementos exteriores
 const blog = (
 	<div>
-		<img src="pics/192940u73.jpg" alt="Trulli" />
-		<h1>Welcome to Dan's Blog!</h1>
+		<img src="pics/192940u73.jpg" alt="Trulli" /> {/* Imagen */}
+		<h1>Welcome to Dan's Blog!</h1> {/* Encabezado del blog */}
 		<article>
 			Wow I had the tastiest sandwich today. I <strong>literally</strong> almost
 			freaked out.
+			{/* Contenido del artículo */}
 		</article>
 	</div>
 );
 
-// Rendering
+// Rendering JSX
+import React from "react"; // Importar React
+import { createRoot } from "react-dom/client"; // Importar método createRoot desde react-dom/client
 
-import React from "react";
-import { createRoot } from "react-dom/client";
-
-const container = document.getElementById("app");
-const root = createRoot(container);
-root.render(<h1>Hello world</h1>);
+const container = document.getElementById("app"); // Obtener el contenedor con ID "app"
+const root = createRoot(container); // Crear una raíz de React
+root.render(<h1>Hello world</h1>); // Renderizar un elemento <h1> dentro del contenedor "app"
