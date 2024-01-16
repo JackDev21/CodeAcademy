9fbe7d329bf04006201b363ec121841f039016ed
JackDev21 <jose.a.c.lopez@gmail.com>
2024-01-16 19:59:02 +0100

    Ejercicio React

---
+++ React/13_React_Component/index.jsx
@@ -0,0 +1,5 @@
+import React from "react"; // Importa la biblioteca React
+import ReactDOM from "react-dom/client"; // Importa la biblioteca ReactDOM para renderizar el componente en el DOM
+import MyComponent from "./app"; // Importa el componente MyComponen
+
+ReactDOM.createRoot(document.getElementById("app")).render(<MyComponent />); // Renderiza el componente MyComponent
