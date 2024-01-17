173a541c5f4f5c82a908d169a07727bae2287886
JackDev21 <jose.a.c.lopez@gmail.com>
2024-01-17 13:14:33 +0100

    react ejercicio 01

--- React/13_React_Component/app.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react"; // Importa la biblioteca React
-
-function MyComponent() {
-	// Definición del componente
-	return <h1>Hello, this is a function component body.</h1>; // Devuelve un elemento <h1> con un mensaje de bienvenida
-}
-
-export default MyComponent;
--- React/13_React_Component/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react"; // Importa la biblioteca React
-import ReactDOM from "react-dom/client"; // Importa la biblioteca ReactDOM para renderizar el componente en el DOM
-import MyComponent from "./app"; // Importa el componente MyComponen
-
-ReactDOM.createRoot(document.getElementById("app")).render(<MyComponent />); // Renderiza el componente MyComponent
