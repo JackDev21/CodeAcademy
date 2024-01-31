7f06b056fce8ac735405e8010a7457933de5ec74
JackDev21 <jose.a.c.lopez@gmail.com>
2024-01-31 16:38:58 +0100

    swetTImeout()

---
+++ AsyncJS_&_HTTPRequest/setTimeout()/01_Ejercicio/main.js
@@ -0,0 +1,15 @@
+// Imprime en la consola el texto "This is the first line of code in app.js."
+console.log("This is the first line of code in app.js.");
+
+// Declara una función llamada 'usingSTO' que imprime el mensaje 'Llegaré a ser programador' en la consola
+const usingSTO = () => {
+    console.log('Llegaré a ser programador');
+}
+
+// Establece un temporizador que ejecutará la función 'usingSTO' después de 3000 milisegundos (3 segundos)
+setTimeout(usingSTO, 3000);
+
+// Imprime en la consola el texto "This is the last line of code in app.js."
+console.log("This is the last line of code in app.js.");
+
+/*El código imprime dos mensajes inmediatamente y establece un temporizador para imprimir un tercer mensaje después de 3 segundos. */
