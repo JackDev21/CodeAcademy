abd0d8987c8f3c903f84f6035db27354dede1ba9
JackDev21 <jose.a.c.lopez@gmail.com>
2024-02-05 17:18:14 +0100

    props

---
+++ React/18_Props_decision/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Greeting from "./Greeting";
+
+function App() {
+	return (
+		<div>
+			<h1>MovieFlix</h1>
+			<Greeting name="Alison" signedIn={true} />
+		</div>
+	);
+}
+
+export default App;
