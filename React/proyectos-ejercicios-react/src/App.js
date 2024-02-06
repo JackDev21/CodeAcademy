f5569205946410348443e98d4adbcc73abb11464
JackDev21 <jose.a.c.lopez@gmail.com>
2024-02-06 20:33:31 +0100

    Cambios en style maquetacion

--- React/proyectos-ejercicios-react/src/App.js
+++ React/proyectos-ejercicios-react/src/App.js
@@ -4,9 +4,10 @@ import Testimonio from './components/Testimonio';
 function App() {
   return (
     <div className="App">
-      <Testimonio />
-
-
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que dicen nuestros alumnos en freeCodeCamp</h1>
+        <Testimonio />
+      </div>
     </div>
   );
 }
