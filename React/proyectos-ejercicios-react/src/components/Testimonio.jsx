f3e97be73a5ac8310ad5ca6ead6736d069c68ea5
JackDev21 <jose.a.c.lopez@gmail.com>
2024-02-07 10:53:24 +0100

    testimonio

--- React/proyectos-ejercicios-react/src/components/Testimonio.jsx
+++ React/proyectos-ejercicios-react/src/components/Testimonio.jsx
@@ -1,24 +1,22 @@
 import React from "react";
+import "../stylesheets/Testimonio.css";
 
-const Testimonio = () => {
+const Testimonio = (props) => {
 	return (
 		<div className="contenedor-testimonio">
 			<img
 				className="imagen-testimonio"
 				alt="Img Emma"
-				src={require("../images/Emma-5a662db9bcf55809a1fefb2ea17634bc.png")}
+				src={require(`../images/testimonio-${props.imagen}.png`)}
 			/>
 			<div className="contenedor-texto-testimonio">
-				<p className="nombre-testimonio">Emma Bostian en Suecia</p>
-				<p className="cargo-testimonio">Ingeniera de Software en Spotify</p>
-				<p className="texto-testimonio">
-					"Siempre he tenido problemas para aprender JavaScript. He tomado
-					muchos cursos, pero el curso de freeCodeCamp fue el que se quedó.
-					Estudiar JavaScript, así como estructuras de datos y algoritmos en
-					freeCodeCamp me dio las habilidades y la confianza que necesitaba para
-					conseguir el trabajo de mis sueños como ingeniero de software en
-					Spotify."
+				<p className="nombre-testimonio">
+					{props.nombre} Bostian en {props.pais}
 				</p>
+				<p className="cargo-testimonio">
+					{props.cargo} en {props.empresa}
+				</p>
+				<p className="texto-testimonio">{props.testimonio}</p>
 			</div>
 		</div>
 	);
